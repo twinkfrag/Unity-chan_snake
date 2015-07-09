716e791f460c2c0d92dbf8586ad90b584ee08dfb
あると <mail@twinkfrag.net>
2015-07-09 18:57:59 +0900

    add Unity-chan & scene

---
+++ Assets/Editor/VstuHook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+using SyntaxTree.VisualStudio.Unity.Bridge;
+using UnityEditor;
+// ReSharper disable PossibleNullReferenceException
+
+namespace Assets.Editor
+{
+	[InitializeOnLoad]
+	public class VstuHook
+	{
+		static VstuHook()
+		{
+			ProjectFilesGenerator.ProjectFileGeneration += (filename, content) => ReplaceProject(content);
+
+		}
+
+		private static string ReplaceProject(string content)
+		{
+			var document = XDocument.Parse(content);
+			var ns = document.Root.Name.Namespace;
+
+			document.Root
+					.Descendants()
+					.Where(x => x.Name.LocalName == "Reference")
+					.Where(x => (string)x.Attribute("Include") == "Boo.Lang")
+					.Remove();
+
+			document.Root
+			        .Descendants()
+			        .First(x => x.Name.LocalName == "PropertyGroup")
+			        .Add(new XElement(ns + "NoWarn", "CSE0001"));
+
+			return document.Declaration + Environment.NewLine + document.Root;
+		}
+	}
+}
---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+// ReSharper disable UnusedMember.Local
+// ReSharper disable ArrangeThisQualifier
+
+namespace Assets.Scripts
+{
+	public class GameMaster : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject localCamera;
+
+		public GameObject LocalCamera
+		{
+			get { return localCamera; }
+			set { localCamera = value; }
+		}
+
+		public static GameObject InheritCamera { get; set; }
+
+		void Start()
+		{
+			Instantiate(InheritCamera ?? LocalCamera).transform.parent = this.transform;
+		}
+	}
+}
